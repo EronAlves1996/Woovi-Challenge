3a9bffa12d35ce511738a2127d86c704fe066d57
Eron Alves <eron.a.silva@capgemini.com>
2022-10-29 18:21:40 -0300

    Refactored Login-Form

--- workspaces/front/src/index/Login-Form.tsx
+++ workspaces/front/src/index/Login-Form.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react';
 import '../App.css'
 import { Form } from './State-Form';
 
 function LoginForm() {
-  const email = Form({type: "email", name:"email"}), password = Form({type: "password", name:"password"})
+  const [email, password] = [Form({type: "email", name:"email", label:"E-mail"}), Form({type: "password", name:"password", label: "Senha"})];
+
   return (
       <form>
-        <label htmlFor="user-email">E-mail</label>
-        {email.getForm()}
-        <label htmlFor="user-password">Senha</label>
-        {password.getForm()}
+        {[email, password].map(el => el.getForm())}
         <button type="button" onClick={() => console.log(email.getState(), password.getState())} >Login</button>
       </form>
   );
--- workspaces/front/src/index/State-Form.tsx
+++ workspaces/front/src/index/State-Form.tsx
@@ -2,15 +2,21 @@ import { useState } from "react";
 
 type FormProps = {
     type: string;
+    label: string;
     name: string;
 }
 
-export function Form(props: FormProps){
+export function Form(props: FormProps) {
     const [thisState, setThisState] = useState("");
 
     return {
-        getForm: ()=>(<input type={props.type} id={props.name} value={thisState} onChange={(e)=>setThisState(e.target.value)}/>),
-        getState: ()=>thisState,
-        setState: (value:string)=>setThisState(value)
+        getForm: () => (
+            <div>
+                <label htmlFor={props.name}>{props.label}</label>
+                <input type={props.type} id={props.name} value={thisState} onChange={(e) => setThisState(e.target.value)} />
+            </div>
+        ),
+        getState: () => thisState,
+        setState: (value: string) => setThisState(value)
     }
 }
