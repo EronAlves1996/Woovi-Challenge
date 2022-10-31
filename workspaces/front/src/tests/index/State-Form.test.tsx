1852396e5330c4f240dbea10f05e182d47e1d1cd
Eron Alves <eron.a.silva@capgemini.com>
2022-10-31 15:25:54 -0300

    Write test case for most types in Form.getForm

--- workspaces/front/src/tests/index/State-Form.test.tsx
+++ workspaces/front/src/tests/index/State-Form.test.tsx
@@ -5,6 +5,7 @@ import { Form } from "../../index/State-Form";
 
 describe("Testing State-Form", () => {
     let container: Element | HTMLElement | null = null;
+    const GenericForm = Form();
 
     beforeEach(() => {
         // setup a DOM element as a render target
@@ -20,11 +21,17 @@ describe("Testing State-Form", () => {
     });
 
     it("should render a form with type text when type is not given", () => {
-        const GenericForm = Form();
         act(() => { render(<GenericForm.getForm name="any" label="any" />, container) });
         expect(container?.getElementsByTagName("input")[0].type).toBe("text");
-    })
+    });
 
+    it("should render types given", ()=>{
+        act(()=> { render(<GenericForm.getForm name="pass" type="password" label="pass" />, container)});
+        expect(container?.querySelector("input")?.type).toBe("password");
+
+        act(()=> render(<GenericForm.getForm name="number" type="number" label="number" />, container));
+        expect(container?.querySelector("input")?.type).toBe("number");
+    })
 })
 
 
